f26dc73759cea93103d80e274b4405784322d89a
Amin <reihani.eng@gmail.com>
2024-07-26 10:28:46 +0330

    Change cookie sameSite to none

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,15 +7,12 @@ module.exports = {
     irPhoneRegEx: /^9(1[0-9]|3[0-9]|2[0-9]|0[0-9]|9[0-9]|4[1])-?[0-9]{3}-?[0-9]{4}$/,
     sessionCookie: {
         development: {
-            domain: 'localhost', 
             path: '/', 
             secure: false,
             httpOnly: true,
             maxAge: 5*24*3600*1000,
-            // sameSite: 'none',
         },
         production: {
-            // domain: 'denooj.com', 
             path: '/', 
             secure: true,
             httpOnly: true,
