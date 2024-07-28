902c3ff2d0ae3b6fd06cb4d78fee86c36ba3b4d7
Amin <reihani.eng@gmail.com>
2024-07-28 15:06:26 +0330

    add a test api coute

--- src/http/controllers/api/web-application/v1/orderController.ts
+++ src/http/controllers/api/web-application/v1/orderController.ts
@@ -110,3 +110,7 @@ export const getUserOrders = async(req: express.Request, res: express.Response) 
         return res.json(results);
     });
 }
+
+export const testRoute = async(req: express.Request, res: express.Response) => {
+    res.json(res);
+}
--- src/routes/api/web-application/v1/public.ts
+++ src/routes/api/web-application/v1/public.ts
@@ -11,7 +11,7 @@ import { registerAttemptValidation, registerSmsCodeValidation } from '../../../.
 import { userProfileValidation, userProfileSmsCodeValidation } from '../../../../helpers/validation/user/userProfileValidation';
 import { loginAttemptValidation, loginSmsCodeValidation } from '../../../../helpers/validation/user/userLoginValidation';
 import { getUserProfile, updateUserProfile, userProfileSmsCodeVerify } from '../../../../http/controllers/api/web-application/v1/userProfileController';
-import { getUserOrderById, getUserOrders } from '../../../../http/controllers/api/web-application/v1/orderController';
+import { getUserOrderById, getUserOrders, testRoute } from '../../../../http/controllers/api/web-application/v1/orderController';
 
 export default (router: express.Router) => {
 
@@ -57,7 +57,5 @@ export default (router: express.Router) => {
     router.get('/order/:id', isUserAuthenticated, getUserOrderById);
     router.get('/orders', isUserAuthenticated, getUserOrders);
 
-    router.get('/test', ($req, $res) => {
-        return $res;
-    });
+    router.get('/test', testRoute);
 }
