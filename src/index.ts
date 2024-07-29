437ffef5fa53cf130f451d9faf8e2bcac4093b3e
Amin <reihani.eng@gmail.com>
2024-07-29 15:56:03 +0330

    Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -43,15 +43,15 @@ const expressApp = express();
 // });
 
 // add rate limiter for api 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	// store: ... , // Redis, Memcached, etc. See below.
+// })
 // Apply the rate limiting middleware to all requests.
-expressApp.use(limiter)
+// expressApp.use(limiter)
 
 
 // set cors
