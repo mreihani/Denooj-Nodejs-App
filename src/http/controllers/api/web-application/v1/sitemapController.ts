9ffe0aeedb7a965f2650634ef1de64885312b648
Amin <reihani.eng@gmail.com>
2024-08-18 16:11:07 +0330

    Fix sitemap permission

--- src/http/controllers/api/web-application/v1/sitemapController.ts
+++ src/http/controllers/api/web-application/v1/sitemapController.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ProductModel, getProducts } from '../../../../../models/product';
 const { simpleSitemapAndIndex } = require('sitemap');
-
+import fs from 'fs';
 
 export const generateSitemaps = async(req: express.Request, res: express.Response) => {
     
@@ -11,6 +11,21 @@ export const generateSitemaps = async(req: express.Request, res: express.Respons
     const products = await getProducts();
 
     try {
+        // Remove the file
+        let siteMapFilePath = './public/sitemaps/sitemap-index.xml';
+        fs.stat(siteMapFilePath, function (err, stats) {
+            console.log(stats);//here we got all information of file in stats variable
+         
+            if (err) {
+                return console.error(err);
+            }
+         
+            fs.unlink(siteMapFilePath,function(err){
+                if(err) return console.log(err);
+                console.log('file deleted successfully');
+            });  
+        });
+
         const sitemap_items = [
             { url: '', priority: 1 },
             { url: 'products' },
@@ -29,6 +44,7 @@ export const generateSitemaps = async(req: express.Request, res: express.Respons
             sourceData: sitemap_items,
             gzip: false, // Disable gzip compression to store sitemaps in .xml format
         });
+
     } catch (error) {
         throw new Error(error);
     }
